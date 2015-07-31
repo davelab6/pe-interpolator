fae09e157dad45101e82cba0de623e47981b3501
Pathum Egodawatta <pathumego@gmail.com>
2015-07-31 15:48:15 +0530

    Changing to weights and off

--- interpolate.py
+++ interpolate.py
@@ -5,19 +5,19 @@ def interpolate( font1, font2, fileName ):
 
     for line in file:
         fontInfo = [ x.strip() for x in line.split( "," ) ]
-        
+
         os.system( "./interpolate.pe " + font1 + " "
                    + font2 + " " + "\"" + fontInfo[0]
                    + "\"" + " " + "\"" + fontInfo[1] + "\""
-                   + " " + "\""  + fontInfo[2] + "\"" 
+                   + " " + "\""  + fontInfo[2] + "\""
                    +  "  "  + "\"" + fontInfo[3] + "\""
                    + " " + fontInfo[4]  )
-        
+
 def main( argv ):
     try:
         interpolate( argv[1], argv[2], argv[3] )
     except:
-        interpolate( argv[1], argv[2], "default-spec" )
+        interpolate( argv[1], argv[2], "weights" )
 
 if __name__ == "__main__":
     main( sys.argv )
